74a5755550e5f481bb977ee5a284e62a1e3358eb
Ishwar Balappanawar <ishwar.balappanawar@students.iiit.ac.in>
2024-12-07 15:40:00 +0530

    fix bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,24 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ishwarbb.github.io',
-  integrations: [tailwind(), image(), mdx()]
+  integrations: [tailwind(), image(), mdx()],
+  content: {
+    collections: {
+      blog: {
+        schema: ({ z }) =>
+          z.object({
+            title: z.string(),
+            pubDate: z.date(),
+            description: z.string(),
+            tags: z.array(z.string()),
+            heroImage: z.string().optional(),
+          }),
+      },
+    },
+  },
 });
